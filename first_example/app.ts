0474d2fab207b197c8bf863d3b40d6bef7b5af0a
Artem Zakholodylo <artem@globaluxsolutions.com>
2016-10-25 13:17:35 +0300

    abstract

--- first_example/app.ts
+++ first_example/app.ts
@@ -1,17 +1,52 @@
 /**
  * name
  */
-class Example {
-    name: string = "Artem";
-
-    constructor(name: string) {
-        this.name = name;
+abstract class Person 
+{
+    firstName: string;
+    lastName: string;
+    fullName: string;
+    age: number;
+    
+    constructor(firstName, lastName) 
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.setFullName();
     }
 
-    helloWorld() {
-        console.log("Hello " + this.name + "!");
+    protected setFullName() 
+    {
+        this.fullName = this.firstName + " " + this.lastName;
+    }
+
+    public setAge(age: number)
+    {
+        this.age = age;
+    }
+
+    abstract getAge():number;
+    abstract sayHi():void;
+}
+
+class Hello extends Person 
+{
+    helloWorld() 
+    {
+        console.log("Hello world!");
+    }
+
+    sayHi():void
+    {
+        console.log("Hi " + this.fullName + "! Your age is " + this.getAge());
+    }
+
+    getAge():number
+    {
+        return this.age;
     }
 }
 
-var example: Example = new Example("Artem");
-example.helloWorld();
+var example: Hello = new Hello("Artem", "Zakholodylo");
+example.setAge(24);
+example.sayHi();
